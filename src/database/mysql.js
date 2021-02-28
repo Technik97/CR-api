b73e7e46ea7be8a7f213afb9ecc9b7c11143dd7f
Technik97 <aniket.97.mane@gmail.com>
2021-02-28 20:50:31 +0530

    Added Database setup

---
+++ src/database/mysql.js
@@ -0,0 +1,11 @@
+require('dotenv').congif()
+
+let config = {
+    client = 'mysql2',
+    host:   process.env.MYSQL_HOST,     
+    user:   process.env.MYSQL_USER,
+    database:   process.env.MYSQL_DB,
+    password:   process.env.MYSQL_PASS
+}
+
+module.exports = require('knex')(config)
