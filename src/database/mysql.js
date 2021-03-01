9d4d810a03c027e956ff5c9c47a9e3e7626a1c0c
Technik97 <aniket.97.mane@gmail.com>
2021-03-01 20:46:10 +0530

    Fixed connection error

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 
 let config = {
     client: 'mysql2',
-    host:   process.env.MYSQL_HOST,     
-    user:   process.env.MYSQL_USER,
-    database:   process.env.MYSQL_DB,
-    password:   process.env.MYSQL_PASS
+    connection:{
+        host:   process.env.MYSQL_HOST,     
+        user:   process.env.MYSQL_USER,
+        database:   process.env.MYSQL_DB,
+        password:   process.env.MYSQL_PASS
+    }
 }
 
 module.exports = require('knex')(config)
